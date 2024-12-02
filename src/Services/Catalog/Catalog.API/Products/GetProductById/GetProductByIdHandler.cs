37a33d5549834f67a3cc9f1e77493cb9b93a53b2
Breno Fortunato <Breno@linksis.com.br>
2024-12-02 07:52:22 -0300

    Generics exceptions

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -10,7 +10,7 @@
             logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
             var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
             if (product is null)
-                throw new ProductNotFoundException();
+                throw new ProductNotFoundException(query.Id);
 
             return new GetProductByIdResult(product);
         }
--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -26,7 +26,7 @@ namespace Catalog.API.Products.UpdateProduct
             logger.LogInformation("UpdateProductCommandHandler.Handle called with {@Command}", command);
             var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
             if (product is null)
-                throw new ProductNotFoundException();
+                throw new ProductNotFoundException(command.Id);
 
             product.Name = command.Name;
             product.Category = command.Category;
