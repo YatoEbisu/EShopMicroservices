181e777a7bed9d445759f0ae8e75e72f5a47ce96
Breno Fortunato <Breno@linksis.com.br>
2024-12-17 08:25:57 -0300

    Configure Grpc enviroment variable in docker compose

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -34,6 +34,14 @@ builder.Services.AddStackExchangeRedisCache(opts =>
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(opts =>
 {
     opts.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() => 
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+    return handler;
 });
 
 
