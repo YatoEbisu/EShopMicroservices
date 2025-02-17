2d858dfdd7b174dc3f7dd389291f489015106b87
Breno Fortunato <BrenoSilvaFortunato@gmail.com>
2025-02-17 08:55:10 -0300

    Configuration database and mapping DDD objects

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).HasConversion(
+                orderItemId => orderItemId.Value,
+                dbId => OrderItemId.Of(dbId));
+
+            builder.HasOne<Product>().WithMany().HasForeignKey(c => c.ProductId);
+
+            builder.Property(c => c.Quantity).IsRequired();
+
+            builder.Property(c => c.Price).IsRequired();
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -7,7 +7,11 @@ namespace Ordering.Infrastructure
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
-            //
+            var connectionString = configuration.GetConnectionString("Database");
+
+            // Add services to the container
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            //services.AddScoped<IApplicationDbContext, ApplicationDbContext>
             return services;
         }
     }
